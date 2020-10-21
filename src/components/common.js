f40b26b44873582dab172f9331ce41b0f958a239
rjrodger <richard@ricebridge.com>
2020-10-21 20:43:30 +0100

    v0.1.0

--- src/components/common.js
+++ src/components/common.js
@@ -1,7 +1,11 @@
 export default {
   clone: (x) => JSON.parse(JSON.stringify(x)),
-  genid: (p) => ((null===p?'':null==p?'ID':p)+Math.random()).replace(/0\./g,''),
-  
+  genid: (p) =>
+    ((null === p ? '' : null == p ? 'ID' : p) + Math.random()).replace(
+      /0\./g,
+      ''
+    ),
+
   taskbox: {
     field: {
       kinds: {
